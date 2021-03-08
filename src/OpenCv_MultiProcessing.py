a559b549c767e61c28c58cb18d14970f952940c3
KadirTaban <kadirtaban08@gmail.com>
2021-03-08 16:04:30 +0300

    explained

--- src/OpenCv_MultiProcessing.py
+++ src/OpenCv_MultiProcessing.py
@@ -6,14 +6,14 @@ import os
 def cam1():
     video_capture = cv2.VideoCapture(0)
 
-    # Initialize variables
+    #face lokasyon listesi
     face_locations = []
 
     while True:
-        # Grab a single frame of video
+
         ret, frame = video_capture.read()
 
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+
         rgb_frame = frame[:, :, ::-1]
 
         # tanımlanan yüzleri yukarda boş bıraktığımız liste lokasyonunun içine attı
@@ -37,28 +37,24 @@ def cam1():
 def cam2():
     video_capture = cv2.VideoCapture("http://192.168.1.103:4747/video")
 
-    # Initialize variables
+
     face_locations = []
 
     while True:
-        # Grab a single frame of video
+
         ret, frame = video_capture.read()
 
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+
         rgb_frame = frame[:, :, ::-1]
 
-        # Find all the faces in the current frame of video
+
         face_locations = face_recognition.face_locations(rgb_frame)
 
-        # Display the results
         for top, right, bottom, left in face_locations:
-            # Draw a box around the face
             cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-        # Display the resulting image
         cv2.imshow('Video', frame)
 
-        # Hit 'q' on the keyboard to quit!
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     video_capture.release()
